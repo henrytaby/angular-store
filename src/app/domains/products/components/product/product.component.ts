352e456c2a809662ada9a48719c9abbc04922d37
Henry Taby <henry.taby@gmail.com>
2025-03-26 09:31:47 -0400

    first commit

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  @Input({required:true}) img: string = '';
+  @Input({required:true}) price: number = 0;
+  @Input({required:true}) title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+  addToCartHandler(){
+    console.log('click form child');
+    this.addToCart.emit('Hola este es un mensaje desde el hijo '+this.title)
+
+  }
+}
---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,84 @@
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required:true}) duration = 0;
+  @Input({required:true}) message = '';
+  counter = signal(0);
+  counterRef : number | undefined;
+
+  constructor() {
+    // NO ASYNC
+    // before render
+    console.log('Constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    // before and during render
+    console.log('Changes');
+    console.log('-'.repeat(10));
+    console.log(changes);
+    const duration = changes['duration'];
+    if(duration && duration.currentValue !== duration.previousValue){
+      this.doSomething();
+    }
+
+    console.log('-'.repeat(10));
+  }
+
+  ngOnInit() {
+    // after render
+    // Una vez
+    // Async, then, subscribe
+    console.log('Init');
+    console.log('-'.repeat(15));
+    console.log('Duration:', this.duration);
+    console.log('Message:', this.message);
+    console.log('-'.repeat(15));
+
+    this.counterRef = window.setInterval(() => {
+      console.log('Run Counter:');
+      this.counter.update(statPrev => statPrev + 1);
+    }
+    , 1000);
+
+  }
+
+  ngAfterViewInit() {
+    // after render - despues de ngOnInit()
+    // despues de renderizar los hijos - si los hijos ya fueron renderizados
+    // Una vez
+    // Async, then, subscribe
+    console.log('After View Init');
+    console.log('-'.repeat(15));
+    console.log('Duration:', this.duration);
+    console.log('Message:', this.message);
+    console.log('-'.repeat(15));
+  }
+  ngOnDestroy() {
+    // before destroy
+    // Una vez
+    // Async, then, subscribe
+    console.log('Destroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething(){
+    console.log('Change Duration');
+  }
+
+
+
+
+}
+function Interval(arg0: () => void, arg1: number) {
+  throw new Error('Function not implemented.');
+}
+
