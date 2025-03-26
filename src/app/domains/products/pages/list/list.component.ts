33c0bc314cd71c50d88f2dcd61615c7944406679
Henry Taby <henry.taby@gmail.com>
2025-03-26 15:07:00 -0400

    refactor: simplify cart management by introducing CartService. Fix prop drilling

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent} from './../../components/product/product.component';
 import { Product } from './../../../shared/models/product.model';
 import { HeaderComponent } from "../../../shared/components/header/header.component";
+import { CartService } from '../../../shared/services/cart.service';
 
 @Component({
   selector: 'app-list',
@@ -13,7 +14,7 @@ import { HeaderComponent } from "../../../shared/components/header/header.compon
 export class ListComponent {
 
   products = signal<Product[]>([]);
-  cart = signal<Product[]>([]);
+  private cartService = inject(CartService);
 
   constructor(){
     const initProducts: Product[] = [
@@ -64,6 +65,6 @@ export class ListComponent {
     this.products.set(initProducts);
   }
   addToCart(product: Product){
-    this.cart.update(prevState => [...prevState, product]);
+    this.cartService.addToCart(product);
   }
 }
