ddb0ff8cc6a9aa585f168528680972af315aecb2
Henry Taby <henry.taby@gmail.com>
2025-03-26 10:10:13 -0400

    about us

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CounterComponent } from "../../../shared/components/counter/counter.component";
+import { WaveAudioComponent } from "../../../info/components/wave-audio/wave-audio.component";
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, CounterComponent],
+  imports: [CommonModule, CounterComponent, WaveAudioComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
