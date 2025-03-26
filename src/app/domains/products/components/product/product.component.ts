3ee820349103ff5e9e30e61dd18632dd9badc0ad
Henry Taby <henry.taby@gmail.com>
2025-03-26 16:13:56 -0400

    feat: add reverse and time-ago pipes for enhanced product display

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { UpperCasePipe, CurrencyPipe, DatePipe } from '@angular/common';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
+import {TimeAgoPipe} from '@shared/pipes/time-ago.pipe';
 @Component({
   selector: 'app-product',
-  imports: [UpperCasePipe, CurrencyPipe, DatePipe],
+  imports: [UpperCasePipe, CurrencyPipe, DatePipe, ReversePipe, TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
