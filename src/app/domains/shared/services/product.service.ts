c992a397f049143cdcb62b938ea83146ed1f1aad
Henry Taby <henry.taby@gmail.com>
2025-03-26 15:48:56 -0400

    refactor: update import paths to use aliases for better readability and maintainability

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { Product } from '../models/product.model';
+import { Product } from '@shared/models/product.model';
 
 @Injectable({
   providedIn: 'root'
