287a8c14d6a96371bbfd1e76fc63b6b09c69a771
Henry Taby <henry.taby@gmail.com>
2025-03-27 19:45:59 -0400

    feat: enhance about component with signal-based message handling and emit functionality

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-
-import { CounterComponent } from '@shared/components/counter/counter.component';
-import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
-import { HighlightDirective } from '@shared/directives/highlight.directive';
 import { FormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop'
+import { CounterComponent } from '@shared/components/counter/counter.component';
+import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
+import { HighlightDirective } from '@shared/directives/highlight.directive';
+
+import { BehaviorSubject, delay, Subject } from 'rxjs';
 @Component({
   selector: 'app-about',
   imports: [CounterComponent, WaveAudioComponent, HighlightDirective, FormsModule],
@@ -14,6 +16,16 @@ export default class AboutComponent {
   duration = signal(1000);
   message = signal('hola');
 
+  obsWithInit$ = new BehaviorSubject<string>('init value ----');
+  $withInit = toSignal(this.obsWithInit$,{
+    requireSync: true,
+  });
+
+  obsWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.obsWithoutInit$.pipe(delay(3000)),{
+    initialValue: '----'
+  });
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration.set(input.valueAsNumber);
@@ -21,7 +33,13 @@ export default class AboutComponent {
 
   changeMessage(event: string) {
     console.log('changeMessage', event);
-    //const input = event.target as HTMLInputElement;
-    //this.message.set(input.value);
+  }
+
+  emitWithInit(){
+    this.obsWithInit$.next('new Value');
+  }
+
+  emitWithoutInit(){
+    this.obsWithoutInit$.next('******');
   }
 }
