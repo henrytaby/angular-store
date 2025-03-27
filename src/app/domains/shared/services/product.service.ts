c596d1fc48f53404aa0b9fe48e8dfdfb6cda3e22
Henry Taby <henry.taby@gmail.com>
2025-03-27 14:13:33 -0400

    feat: update environment imports to use new path alias for improved clarity

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Product } from '@shared/models/product.model';
-import { environment } from '../../../../environments/environment';
+import { environment } from '@env/environment';
 @Injectable({
   providedIn: 'root',
 })
